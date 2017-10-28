eefaa472fa28af149870ce3bc171adc6be4507e3
mavx <auyongminhao@gmail.com>
2017-10-28 16:54:41 +0800

    Adding transactions to block

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,36 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
     
-    def new_block(self):
-        # Creates a new Block and adds it to the chain
-        pass
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new Block in the Blockchain
+
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
+
+        # Reset the current list of transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
     
     def new_transaction(self, sender, recipient, amount):
         """
