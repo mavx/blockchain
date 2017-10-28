76a038c7bf744116017c5a2ce61f46b7f645d735
mavx <auyongminhao@gmail.com>
2017-10-28 17:01:18 +0800

    Creating new blocks

--- blockchain.py
+++ blockchain.py
@@ -51,13 +51,20 @@ class Blockchain(object):
     
     @staticmethod
     def hash(block):
-        # Hashes a Block
-        pass
+        """
+        Creates a SHA-256 hash of a Block
+
+        :param block: <dict> Block
+        :return: <str>
+        """
+        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
     
     @property
     def last_block(self):
         # Returns the last Block in the chain
-        pass
+        return self.chain[-1]
 
 block = {
     'index': 1,
